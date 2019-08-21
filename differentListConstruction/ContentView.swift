62514bd12621839f9124fc39830f7c1c076a3cdc
Steedman Bass <steedman.bass@gmail.com>
2019-08-21 12:19:18 -0400

    Initial Commit

---
+++ differentListConstruction/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  differentListConstruction
+//
+//  Created by Steedman Bass on 8/21/19.
+//  Copyright © 2019 Steedman Bass. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
